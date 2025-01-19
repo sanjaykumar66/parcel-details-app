aa77d455524672dfc9be188953e99f64de07dc83
sanjay-HL <sanjay.kumar@gohighlevel.com>
2025-01-19 23:25:52 +0530

    signout added

--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { NCard, NPopover, NAvatar, NButton } from 'naive-ui';
+import { auth } from '../config/firebaseConfig'
+
+const kickOut = () => {
+    auth.signOut()
+}
 </script>
 <template>  
     <NCard class="shadow-lg" content-class="!py-3 flex justify-between items-center">
@@ -8,14 +13,15 @@ import { NCard, NPopover, NAvatar, NButton } from 'naive-ui';
                 src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
             />
+            
             <NPopover>
                 <template #trigger>
                     <div class="flex items-center cursor-pointer">
-                        <NAvatar round  size="medium">AN</NAvatar>
+                        <NAvatar round  size="medium">{{ auth?.currentUser?.email?.slice(0,2).toUpperCase() }}</NAvatar>
                     </div>
                 </template>
                 <div>
-                    <NButton quaternary type="default">Logout</NButton>
+                    <NButton @click="kickOut" quaternary type="default">Logout</NButton>
                 </div>
             </NPopover>
     </NCard>
